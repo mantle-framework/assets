a2d0dc91685c461cbb85d4cc7935f6fb59865b4a
mantle-ci <mantle@alley.co>
2022-04-08 18:14:43 +0000

    Adding check for empty repo

---
+++ class-asset-manager.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * Asset_Manager class file.
+ *
+ * phpcs:disable Squiz.Commenting.FunctionComment
+ *
+ * @package Mantle
+ */
+
+namespace Mantle\Assets;
+
+use Asset_Manager_Preload;
+use Asset_Manager_Scripts;
+use Mantle\Contracts\Assets\Asset_Manager as Asset_Manager_Contract;
+use Mantle\Contracts\Assets\Load_Method;
+
+/**
+ * Asset Manager
+ */
+class Asset_Manager implements Asset_Manager_Contract {
+	/**
+	 * Load a external script.
+	 *
+	 * @param string          $handle Script handle.
+	 * @param string          $src Script URL, optional.
+	 * @param string[]|string $deps Script dependencies, optional.
+	 * @param array|string    $condition Condition to load, defaults to global.
+	 * @param string          $load_method Load method.
+	 * @param string          $load_hook Load hook.
+	 * @param string|null     $version Script version.
+	 * @return Asset
+	 */
+	public function script( ...$params ) {
+		return new Asset( 'script', ...$params );
+	}
+
+	/**
+	 * Load an external stylesheet file.
+	 *
+	 * @param string          $handle Stylesheet handle.
+	 * @param string          $src Stylesheet URL, optional.
+	 * @param string[]|string $deps Stylesheet dependencies.
+	 * @param array|string    $condition Condition to load.
+	 * @param string          $load_method Load method.
+	 * @param string          $load_hook Load hook.
+	 * @param string|null     $version Script version.
+	 * @return void
+	 */
+	public function style( ...$params ): Asset {
+		return new Asset( 'style', ...$params );
+	}
+
+	/**
+	 * Preload content by URL.
+	 *
+	 * @link https://developer.mozilla.org/en-US/docs/Web/HTML/Link_types/preload
+	 *
+	 * @param string      $handle Preload handle.
+	 * @param string      $src URL to preload.
+	 * @param string      $condition Condition to preload.
+	 * @param string|null $as Preload as, defaults to detect "as" by file URL.
+	 * @param string|null $mime_type Mime type to load as, defaults to detect by file URL.
+	 * @param string      $media Media to preload, defaults to 'all'.
+	 * @param bool        $crossorigin Flag to load as cross origin, defaults to false.
+	 * @param string|null $version Handle version, optional.
+	 * @return void
+	 */
+	public function preload(
+		string $handle,
+		string $src,
+		$condition = 'global',
+		?string $as = null,
+		?string $mime_type = null,
+		string $media = 'all',
+		bool $crossorigin = false,
+		?string $version = null
+	): void {
+		Asset_Manager_Preload::instance()->add_asset(
+			[
+				'handle'      => $handle,
+				'src'         => $src,
+				'condition'   => $condition,
+				'version'     => $version,
+				'media'       => $media,
+				'as'          => $as,
+				'crossorigin' => $crossorigin,
+				'mime_type'   => $mime_type,
+			]
+		);
+	}
+
+	/**
+	 * Asynchronously load a script file.
+	 *
+	 * @param string $handle Handle to change.
+	 * @return void
+	 */
+	public function async( string $handle ): void {
+		Asset_Manager_Scripts::instance()->modify_load_method( $handle, Load_Method::ASYNC );
+	}
+
+	/**
+	 * Defer a script file
+	 *
+	 * @param string $handle Handle to change.
+	 * @return void
+	 */
+	public function defer( string $handle ): void {
+		Asset_Manager_Scripts::instance()->modify_load_method( $handle, Load_Method::DEFER );
+	}
+
+	/**
+	 * Change the load method of an asset.
+	 *
+	 * @param string $handle Handle to change.
+	 * @param string $load_method Load method to change to.
+	 * @return void
+	 */
+	public function load_method( string $handle, string $load_method = Load_Method::SYNC ): void {
+		Asset_Manager_Scripts::instance()->modify_load_method( $handle, $load_method );
+	}
+}
---
+++ class-asset-service-provider.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Asset_Service_Provider class file.
+ *
+ * @package Mantle
+ */
+
+namespace Mantle\Assets;
+
+use Mantle\Support\Service_Provider;
+
+/**
+ * Asset Service Provider
+ */
+abstract class Asset_Service_Provider extends Service_Provider {
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		$this->app->singleton(
+			'asset.manager',
+			fn() => new Asset_Manager(),
+		);
+
+		$this->app->alias( 'asset.manager', Asset_Manager::class );
+		$this->app->alias( 'asset.manager', \Mantle\Contracts\Assets\Asset_Manager::class );
+	}
+}
